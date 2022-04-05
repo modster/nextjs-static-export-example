7fb4de79aa4a5eea84d994bada0329f45bd8c3c4
E. M. Greeff <unmodster@gmail.com>
2022-04-05 03:24:49 -0600

    reconfig runner

--- .next.config.js
+++ .next.config.js
@@ -1,14 +1,4 @@
 const nextConfig = {
   reactStrictMode: true,
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-      '/post/1': { page: '/post', query: { title: '1' } },
-    }
-  },
 }
-
 module.exports = nextConfig
