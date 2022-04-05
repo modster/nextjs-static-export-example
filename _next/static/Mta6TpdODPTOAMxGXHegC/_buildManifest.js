d291d04181d518527a0db53306b4ee8f02ba2bff
modster <modster@users.noreply.github.com>
2022-04-05 05:43:30 -0600

    deploy: 96abc1520a8203fd86538b8b990e9e33e7cee661

---
+++ _next/static/Mta6TpdODPTOAMxGXHegC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-87f6dffb90866180.js"],"/_error":["static/chunks/pages/_error-e526a695839b5a36.js"],"/post/[id]":["static/chunks/pages/post/[id]-add702236d875952.js"],sortedPages:["/","/_app","/_error","/post/[id]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
